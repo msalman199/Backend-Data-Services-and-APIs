f088a5974605dc449a2569198233f47ff493b1ca
Hafiz Muhammad Salman <hafizmuhammadsalman13@gmail.com>
2026-06-02 17:50:38 -0700

    Create cpu_stress.sh

---
+++ Alert Rule Implementation/tmp/cpu_stress.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# TODO: Create CPU stress test
+# Generate load on all CPU cores for testing
+
+echo "Starting CPU stress test..."
+# TODO: Implement stress test using yes command or similar
+# Hint: yes > /dev/null & (run multiple times)
+
+# TODO: Store PIDs for cleanup
+# TODO: Wait for specified duration
+# TODO: Kill processes
+
+echo "CPU stress test completed"
